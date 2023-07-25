9f8ddfe57024be96ddc6cf784bf93eeec297705e
MeganKullu <kullumegan@gmail.com>
2023-07-25 22:55:38 +0300

    ui done

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'light-cyan':'#CEE3E9',
+        'neon-green':'#52FFA8',
+        'grayish-blue':'#4E5D73',
+        'dark-grayish-blue':'#323A49',
+        'dark-blue':'#1F2632',
+
+      }
+    },
+  },
+  plugins: [],
+}
+
